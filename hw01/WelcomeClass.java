8edf52760281befaf1e4b98f6d6376f43d12fcaf
var217 <var217@lehigh.edu>
2014-09-02 00:01:58 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,35 @@
+////////////////////////////////////////////////////////////////
+//Victoria Rodrigues
+//August 31 2014
+//Welcome Class Program
+//
+//  first compile the program
+//      javac WelcomeClass.java
+//  run the program
+//      java WelcomeClass//
+
+//  define a class
+public class WelcomeClass 
+{
+
+// add main method
+ public static void main(String [] args) 
+ {
+     
+//  print welcome message with decorative box and tweet
+    System.out.println("-----------");
+    System.out.println("| WELCOME |");
+    System.out.println("___________");
+    System.out.println("  ^  ^  ^  ^  ^  ^");
+    System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+    System.out.println("|-V--A--R--2--1--7-|");
+    System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+    System.out.println("  v  v  v  v  v  v");
+    System.out.println("This past June, I won super VIP tickets to the Firefly Festival.");
+    
+    //test/testtest
+    }
+}
+    
+ 
+
